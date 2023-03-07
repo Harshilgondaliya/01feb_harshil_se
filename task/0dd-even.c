dec4a2b2d2faa0007555c7103b3cc1a205b25f37
Harshilgondaliya <124335791+Harshilgondaliya@users.noreply.github.com>
2023-03-07 11:37:37 +0530

    Add files via upload

---
+++ task/0dd-even.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter the even value:");
+    scanf("%d",&a);
+    if (a % 2 == 0)
+    {
+        printf("\n value is even");
+    }
+    else
+    {
+        printf("\n value is odd");
+    }
+    
+}
