9d720a583f201b83193bd9d566c8f7909cae5889
Harshilgondaliya <124335791+Harshilgondaliya@users.noreply.github.com>
2023-03-07 11:32:52 +0530

    Add files via upload

---
+++ task/if.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter the even value:");
+    scanf("%d",&a);
+    if (a % 3 == 0)
+    {
+        printf("\n value is even");
+    }
+    
+}
