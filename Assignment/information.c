7adf0a60ba571396af8cc724a67e2564b1276f7d
Harshilgondaliya <124335791+Harshilgondaliya@users.noreply.github.com>
2023-03-09 10:38:59 +0530

    Delete information.c

--- Assignment/information.c
+++
@@ -1,9 +0,0 @@
-//Display This Information using printf//
-#include<stdio.h>
-void main()
-{
-    printf("Name:Harshil Gondaliya");
-    printf("\nBirth Date:15/11/2003");
-    printf("\nAge:20");
-    printf("\nAddress:Adarsh city,Rajkot,mavdi");
-}
