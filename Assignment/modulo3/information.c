7975dcd5daad69c3c7340d484dd93b356f6db13e
Harshilgondaliya <124335791+Harshilgondaliya@users.noreply.github.com>
2023-03-09 10:38:28 +0530

    Add files via upload

---
+++ Assignment/modulo3/information.c
@@ -0,0 +1,9 @@
+//Display This Information using printf//
+#include<stdio.h>
+void main()
+{
+    printf("Name:Harshil Gondaliya");
+    printf("\nBirth Date:15/11/2003");
+    printf("\nAge:20");
+    printf("\nAddress:Adarsh city,Rajkot,mavdi");
+}
