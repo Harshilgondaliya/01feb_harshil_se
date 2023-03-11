0e4f64aff3596368d4c3299b1596fde0dd4183f2
Harshilgondaliya <124335791+Harshilgondaliya@users.noreply.github.com>
2023-03-11 11:31:52 +0530

    Add files via upload

---
+++ task/conditinol.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+    int a,b;
+    printf("enter the value of 2 number:");
+    scanf("%d%d",&a,&b);
+    (a<b) ? (printf("A is small")) : (printf("B is small"));
+    
+}
