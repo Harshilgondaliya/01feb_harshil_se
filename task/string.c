b3e2131385966136df8920dde11dfcb2cfb43f33
Harshilgondaliya <124335791+Harshilgondaliya@users.noreply.github.com>
2023-03-25 10:35:21 +0530

    Add files via upload

---
+++ task/string.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    char name[10];//={"abcde"};
+    //printf("\nstring is:%s",str);
+    printf("\nEnter your name :");
+    scanf("%s",&name);
+    printf("\nGood Morning %s",name);
+}
