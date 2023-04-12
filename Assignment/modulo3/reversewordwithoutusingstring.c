55d4168a7b8bfaa3db35c004da2f45959ee0e58e
Harshilgondaliya <124335791+Harshilgondaliya@users.noreply.github.com>
2023-04-12 12:32:22 +0530

    Add files via upload

---
+++ Assignment/modulo3/reversewordwithoutusingstring.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char name[10];
+    printf("Enter Your Name : ");
+    scanf("%s",&name);
+    int length;
+    length = strlen(name);
+    int i;
+    for ( i = length; i >=0; i--)
+    {
+        printf("%c",name[i]);
+    }
+    int a = 0;
+    for ( i =length; i ; i--)
+    {
+        a++;
+    }
+    printf("\n%d",a);
+    
+    
+}
