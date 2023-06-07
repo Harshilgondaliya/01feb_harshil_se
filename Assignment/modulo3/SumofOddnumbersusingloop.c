d23c6ae9302500db43948597d83abc241572a30d
Harshilgondaliya <124335791+Harshilgondaliya@users.noreply.github.com>
2023-06-07 12:08:18 +0530

    Add files via upload

--- Assignment/modulo3/SumofOddnumbersusingloop.c
+++ Assignment/modulo3/SumofOddnumbersusingloop.c
@@ -4,27 +4,21 @@ int main()
 {
     int number;
     int sum = 0;
-
-    // enter number from find odd number 
+ 
     printf("Enter odd number 1 to : ");
     scanf("%d",&number);
 
-    // show even number
     printf("odd numbers from 1 to %d is : ",number);
 
     for (int i = 1; i < number ; i++)
     {
-
-        // check odd number
         if (i % 2 != 0)
         {
             printf("%d, ",i);
             sum = sum + i;
         }
-        
     }
-
-    // print sum of even number
+    
     printf("\nSum of All odd Numbers from 1 to %d is : %d ",number,sum);
     
     
